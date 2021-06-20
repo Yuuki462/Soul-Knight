77d3548d398d126a11ecf59603728ac054f86f01
qu-ge-ming-zi-tai-nan-le <83444367+qu-ge-ming-zi-tai-nan-le@users.noreply.github.com>
2021-06-20 20:04:05 +0800

    Add files via upload

---
+++ Classes/Item/Item.cpp
@@ -0,0 +1,42 @@
+﻿#include "Item.h"
+
+void Item::bindSprite(Sprite* pSprite)
+{
+    m_pSprite = pSprite;
+    this->addChild(m_pSprite);
+}
+
+Sprite* Item::getSprite()
+{
+    return m_pSprite;
+}
+
+ui::Scale9Sprite* Item::showInformation(const std::string& message)
+{
+	auto board = ui::Scale9Sprite::create("interface/voidboard.png");
+	board->retain();
+	board->setContentSize(Size(message.size() * 5.0f, 40.0f));
+
+	auto setInformation = Label::createWithTTF(message, "Font/IRANYekanBold.ttf", 20.);
+	const Vec2 informationPosition = { message.size() * 2.5f,25.0f };
+
+	setInformation->setPosition(informationPosition);
+	board->addChild(setInformation, 3);
+
+	return board;
+}
+
+void Item::remove()
+{
+	m_pSprite->removeFromParentAndCleanup(true);
+}
+
+void Item::changeSprite(Sprite* _sprite)
+{
+	m_pSprite = _sprite;
+}
+
+void Item::setInformationUnvisible()
+{
+	m_pInformation->setVisible(false);
+}
---
+++ Classes/Item/Item.h
@@ -0,0 +1,32 @@
+﻿#ifndef _ITEM_H_
+#define _ITEM_H_
+
+#include"cocos2d.h"
+#include "ui/CocosGUI.h"
+using namespace cocos2d;
+
+class Item :public Node
+{
+public:
+	CREATE_FUNC(Item);
+
+
+	void bindSprite(Sprite* pSprite);
+
+	Sprite* getSprite();
+
+	ui::Scale9Sprite* showInformation(const std::string& message);
+
+	void remove();
+
+	void changeSprite(Sprite* _sprite);
+
+	void setInformationUnvisible();
+
+protected:
+	Sprite* m_pSprite=nullptr;
+	ui::Scale9Sprite* m_pInformation;
+
+};
+
+#endif
